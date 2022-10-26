95b07872c0c2ad799dc7341a59002ab41c89d565
Anna <anyaamdmitrieva@yandex.ru>
2022-10-26 13:41:06 +0600

    add App3

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] array = { 8, 9, 5, 6, 47, 4, 6, 9, 78, 1125 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPos = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPos]) maxPos = j;
+        }
+
+        int temp = array[i];
+        array[i] = array[maxPos];
+        array[maxPos] = temp;
+    }
+}
+PrintArray(array);
+
+SelectionSort(array);
+
+PrintArray(array);
+
+
